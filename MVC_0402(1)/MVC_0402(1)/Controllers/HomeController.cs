ffe3448de4bf587e6d18d054bc8b68615c31bd3b
Trotter-Stewart <Trotter.G.Stewart-1@ou.edu>
2024-04-01 19:42:58 -0500

    MVC_0402(1)

---
+++ MVC_0402(1)/MVC_0402(1)/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_0402_1_.Data;
+
+namespace MVC_0402_1_.Controllers
+{
+    public class HomeController : Controller
+    {
+        StudentDB db = new StudentDB();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GD()
+        {
+            return Json(db.Students);
+        }
+    }
+}
---
+++ MVC_0402(1)/MVC_0402(1)/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_0402_1_.Models;
+
+[Table("students")]
+public partial class Student
+{
+    [Key]
+    public string Id { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public int Age { get; set; }
+
+    public double Grade { get; set; }
+
+    public string LetterGrade { get; set; } = null!;
+}
